340ed9eefa8fd5aa9a78db471b5dbe20341c439e
Jacob Walton <jawalton6616@gmail.com>
2021-08-19 19:56:11 -0500

    finished studio

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -1,31 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-equipment',
-  templateUrl: './equipment.component.html',
-  styleUrls: ['./equipment.component.css']
+  selector: "app-equipment",
+  templateUrl: "./equipment.component.html",
+  styleUrls: ["./equipment.component.css"],
 })
 export class EquipmentComponent implements OnInit {
-   equipmentItems: object[] = [
-       {name: 'Duct Tape', mass: 0.5},
-       {name: 'Space Camera', mass: 20},
-       {name: 'Food', mass: 150},
-       {name: 'Oxygen Tanks', mass: 400},
-       {name: 'AE-35 Unit', mass: 5},
-       {name: 'ISS Supplies', mass: 800},
-       {name: 'Water', mass: 250},
-       {name: 'Satellite', mass: 1200},
-       {name: 'R2 Unit', mass: 32}
-   ];
-   cargoHold: object[] = [];
-   cargoMass: number = 0;
-   maximumAllowedMass: number = 2000;
-   maxItems: number = 10;
+  equipmentItems: object[] = [
+    { name: "Duct Tape", mass: 0.5 },
+    { name: "Space Camera", mass: 20 },
+    { name: "Food", mass: 150 },
+    { name: "Oxygen Tanks", mass: 400 },
+    { name: "AE-35 Unit", mass: 5 },
+    { name: "ISS Supplies", mass: 800 },
+    { name: "Water", mass: 250 },
+    { name: "Satellite", mass: 1200 },
+    { name: "R2 Unit", mass: 32 },
+  ];
+  cargoHold: object[] = [];
+  cargoMass: number = 0;
+  maximumAllowedMass: number = 2000;
+  maxItems: number = 10;
+  crew = [];
+  isDisabled: boolean = false;
+  checkDisabled(item) {
+    if (
+      this.cargoHold.length == this.maxItems ||
+      this.cargoMass + item.mass > 2000
+    ) {
+      this.isDisabled = true;
+    }
+  }
 
-   constructor() { }
+  isNearMaxMass: boolean = false;
+  checkIfNear() {
+    if (2000 - this.cargoMass <= 200) {
+      this.isNearMaxMass = true;
+    }
+  }
 
-   ngOnInit() { }
+  clearHold() {
+    this.cargoHold = [];
+    this.cargoMass = 0;
+  }
 
-   // Code your addItem function here:
-   
+  constructor() {}
+
+  ngOnInit() {}
+
+  // Code your addItem function here:
+  addItem(equipment) {
+    this.cargoHold.push(equipment);
+    this.cargoMass = this.cargoMass + equipment.mass;
+    if (this.maximumAllowedMass - this.cargoMass <= 200) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
